a2a6e7968978a5fe22a6239da149ce471391193b
Sha Zhang <e0000378@u.nus.edu>
2015-08-15 15:51:02 +0800

    Completed Excercise 1

--- CSExercises/Ex07-15/Ex13.cs
+++ CSExercises/Ex07-15/Ex13.cs
@@ -11,11 +11,12 @@ namespace CSExercises
         public static void Main(string[] args)
         {
             Console.WriteLine("Please enter a distane:");
-            double x, Fare, Fare1;
+            double x, Fare, Fare1,Fare2;
             x = Convert.ToDouble(Console.ReadLine());
             Fare = 2.4 + 3.2 * x;
-            Fare1 = Math.Ceiling(Fare);
-            Console.WriteLine("The total fare is {0:c}", Fare1);
+            Fare1 = Math.Ceiling(Fare * 10);
+            Fare2 = Fare1 / 10;
+            Console.WriteLine("The total fare is {0:c}", Fare2);
             Console.ReadKey();
         }
     }
